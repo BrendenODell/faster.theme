763d17ca8997a90b4dc1aeaee89c57e839cb7556
james <james@mostoptimal.com>
2016-07-29 12:15:44 -0400

    remove wp files

--- wordfence-waf.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
-
-if (file_exists('/var/www/faster.support/wp-content/plugins/wordfence/waf/bootstrap.php')) {
-	define("WFWAF_LOG_PATH", '/var/www/faster.support/wp-content/wflogs/');
-	include_once '/var/www/faster.support/wp-content/plugins/wordfence/waf/bootstrap.php';
-}
-?>
