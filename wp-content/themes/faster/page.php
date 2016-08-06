bd7f3bf7ba1bc7da985bf72a16b3b0b388ce45de
James <james@mostoptimal.com>
2016-08-06 15:43:33 -0400

    added sign-up page template and some additional styling

--- wp-content/themes/faster/page.php
+++ wp-content/themes/faster/page.php
@@ -16,8 +16,6 @@
 
 get_header(); ?>
 <?php get_template_part( 'content', 'header' ); ?>
-	<div class="container">
-		<div class="center-block">
 		<?php
 	    // TO SHOW THE PAGE CONTENTS
 	    while ( have_posts() ) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
@@ -26,6 +24,4 @@ get_header(); ?>
 	    endwhile; //resetting the page loop
 	    wp_reset_query(); //resetting the page query
 	    ?>
-	    </div>
-	</div>	 
 <?php get_footer(); ?>
